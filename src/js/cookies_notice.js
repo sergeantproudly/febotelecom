6a6deef8b5a3282fbfdc6a12821462e82ac829d1
romanproudly <roman@proudly.ru>
2020-07-21 15:32:25 +0300

    cookies timeout

--- src/js/cookies_notice.js
+++ src/js/cookies_notice.js
@@ -38,12 +38,14 @@ $(document).ready(function() {
 		var noticed = getCookie('cookie_notice');
 		if (typeof(noticed) == 'undefined' || noticed != 1) {
 			$('body').append('<div id="cookies-notice" style="display:none;"><div class="wrapper"><p>This site uses cookies to give you a better experience. Read more about <a href="terms-conditions.html" target="_blank">cookies policy</a></p><button class="btn js-ok"><span>Accept and close</span></button></div></div>');
-			$('#cookies-notice').stop().fadeIn(700);
-			$('#cookies-notice .js-ok').click(function() {
-				setCookie('cookie_notice', 1);
-				$('#cookies-notice').stop().fadeOut(700, function() {
-					$('#cookies-notice').remove();
+			setTimeout(function() {
+				$('#cookies-notice').stop().fadeIn(700);
+				$('#cookies-notice .js-ok').click(function() {
+					setCookie('cookie_notice', 1);
+					$('#cookies-notice').stop().fadeOut(700, function() {
+						$('#cookies-notice').remove();
+					});
 				});
-			});
+			}, 4000);			
 		}
 });
