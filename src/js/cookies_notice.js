e3fc12de9d55f749e9a197edc6c38c545c541bfa
romanproudly <roman@proudly.ru>
2020-08-03 21:53:15 +0300

    policies update

--- src/js/cookies_notice.js
+++ src/js/cookies_notice.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	var noticed = $.cookie('cookie_notice');
 	if (typeof(noticed) == 'undefined' || noticed != 1) {
-		$('body').append('<div id="cookies-notice" style="display:none;"><div class="wrapper"><p>This site uses cookies to give you a better experience. Read more about <a href="terms-conditions.html" target="_blank">cookies policy</a></p><button class="btn js-ok"><span>Accept and close</span></button></div></div>');
+		$('body').append('<div id="cookies-notice" style="display:none;"><div class="wrapper"><p>This site uses cookies to give you a better experience. Read more about <a href="cookies-policy.html" target="_blank">cookies policy</a></p><button class="btn js-ok"><span>Accept and close</span></button></div></div>');
 		setTimeout(function() {
 			$('#cookies-notice').stop().fadeIn(700);
 			$('#cookies-notice .js-ok').click(function() {
